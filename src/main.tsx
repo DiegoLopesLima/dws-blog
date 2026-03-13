7587da0013462513cf30979a8ab1874a70f92e95
Diego Lopes Lima <web.diego.lima@gmail.com>
2026-03-13 17:08:33 -0300

    feat: reset css and set up initial tokens

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/600.css";
+import "@fontsource/open-sans/700.css";
 import "./index.scss";
 import App from "./App.tsx";
 
