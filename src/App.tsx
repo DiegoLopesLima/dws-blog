ea1b7013ca3da5bb18f1cc1db0c0804b5b904bbf
Diego Lopes Lima <web.diego.lima@gmail.com>
2026-03-13 14:51:01 -0300

    feat: show posts titles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getPosts } from "@/services/posts";
+import type { Post } from "./types/post";
 
 function App() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
   useEffect(() => {
     getPosts().then((posts) => {
-      console.log(posts);
+      setPosts(posts);
     });
   }, []);
 
-  return <div className="hello">Hello World!</div>;
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
